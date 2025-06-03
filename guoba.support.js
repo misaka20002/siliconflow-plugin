2e6254fba76e5feb83005e6d6a4b874721d05ad5
WindyCloud <22683776+WindyCloudCute@users.noreply.github.com>
2025-06-03 19:11:07 +0800

    Update guoba.support.js
    为BOT名称触发配置添加一段更详细的使用解释。

--- guoba.support.js
+++ guoba.support.js
@@ -581,7 +581,7 @@ export function supportGuoba() {
         {
           field: "botName",
           label: "BOT名称",
-          bottomHelpMessage: "设置BOT的名称，当消息中包含这个名称时会触发对话；留空则关闭；更改后重启生效",
+          bottomHelpMessage: "设置BOT的名称，当消息中包含这个名称时会触发对话；有多个名称的情况下使用 | 分隔；留空则关闭；更改后重启生效",
           component: "Input",
           componentProps: {
             placeholder: "小助手",
